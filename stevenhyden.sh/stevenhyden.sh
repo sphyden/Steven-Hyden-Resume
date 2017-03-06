24c8c54607352d22be941a7e6574cca5e7eaf9fe
Steven Hyden <sphyden@gmail.com>
2017-03-05 22:52:00 -0600

    One more

--- stevenhyden.sh/stevenhyden.sh
+++ stevenhyden.sh/stevenhyden.sh
@@ -10,12 +10,12 @@ inc=./text
 dump='less -R -j1'
 
 echo -e "\n"
-echo -e "			        \033[1;36mSteven Hyden\033[0m"
+echo -e "			        \033[1;34mSteven Hyden\033[0m"
 echo -e "\n"
 echo -e "\033[1mHello!\033[0m And thank you for taking this script, and my employment into consideration."
 echo -e "\n"
 echo -e "\033[4mPlease choose one of the following options to navigate through my resume.\033[0m \n 
-\033[4mAfter selecting an option, and surveying its contents, push 'q' to return to the selection prompt.\033[0m"
+\033[4mAfter selecting an option and surveying its contents, push 'q' to return to the selection prompt.\033[0m"
 echo -e "\n"
 
 select opt in "${options[@]}"
