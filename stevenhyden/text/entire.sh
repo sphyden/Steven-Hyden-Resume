6e2e7623d4164e3172aaefeb99acc3bfb8d8817c
shyden <shyden@ephmgmt.com>
2017-03-06 16:04:24 -0600

    Fixed temp file issue with entire.sh

--- stevenhyden/text/entire.sh
+++ stevenhyden/text/entire.sh
@@ -4,15 +4,6 @@
 
 d=./text
 
-#$d/contact.sh;
-#$d/intro.sh;
-#$d/environment.sh;
-#$d/skills.sh;
-#$d/training.sh;
-#$d/experience.sh;
-
 for option in $d/contact.sh $d/intro.sh $d/environment.sh $d/skills.sh $d/training.sh $d/experience.sh;
-  do $option >> /tmp/full.res;
-done
-less -Rj1 /tmp/full.res; wait;
-rm -f /tmp/full.res
+  do $option >&1;
+done | less -Rj1
